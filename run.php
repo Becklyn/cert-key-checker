909cc2bf8eb5a4d0f21b4a5ebc34dd24104a7ee9
Jannik Zschiesche <jz@becklyn.com>
2017-11-22 18:41:58 +0100

    Correctly run autoloader in composer

--- run.php
+++ run.php
@@ -3,7 +3,11 @@
 
 if (is_file($autoloader = __DIR__ . "/vendor/autoload.php"))
 {
-    require $autoloader;
+    require_once $autoloader;
+}
+else if (is_file($autoloader = dirname(__DIR__, 2) . "/autoload.php"))
+{
+    require_once $autoloader;
 }
 
 use Symfony\Component\Console\Application;
